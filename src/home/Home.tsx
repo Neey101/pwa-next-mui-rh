3a05d2d179de9f36cf53bdab0df9d18a9e591cfe
ney <neeycandia1001@gmail.com>
2024-06-27 19:06:49 -0300

    card finalizado

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,11 +1,37 @@
-import { Container } from "@mui/material";
+import {
+  Container,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Card,
+} from "@mui/material";
 import MainSection from "@/src/home/components/MainSection";
 
 const Home = () => {
   return (
     <>
       <Container maxWidth={false}>
-        <MainSection />
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image="https://e1.nmcdn.io/anicira/wp-content/uploads/2021/08/Emergency-cat.png"
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button variant="contained">Siguiente</Button>
+          </CardActions>
+        </Card>
       </Container>
     </>
   );
